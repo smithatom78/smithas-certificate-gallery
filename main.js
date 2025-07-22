b9e97afbc3fc1fcedd56389de905f9587e638de3
Smitha Jacob <smithatom78@gmail.com>
2025-07-22 12:24:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 const scrollDisplay = document.getElementById('imgScroll');
 const displayImg = document.getElementById('imgMain');
 
+// Array of image paths (images are now in the root folder)
 const images = [
-  'https://raw.githubusercontent.com/smithatom78/smithas-certificate-gallery/main/certificate1.png', 
-  'https://raw.githubusercontent.com/smithatom78/smithas-certificate-gallery/main/certificate2.png', 
-  'https://raw.githubusercontent.com/smithatom78/smithas-certificate-gallery/main/certificate3.png',
-  'https://raw.githubusercontent.com/smithatom78/smithas-certificate-gallery/main/certificate4.png',
-  'https://raw.githubusercontent.com/smithatom78/smithas-certificate-gallery/main/certificate5.png'
+  'certificate1.png', 
+  'certificate2.png', 
+  'certificate3.png',
+  'certificate4.png',
+  'certificate5.png'
 ];
 
-// Loop through the image URLs to create image elements
+// Loop through the images and create <img> elements
 for (let i = 0; i < images.length; i++) {
   const newImg = document.createElement('img');
-  newImg.src = images[i];
+  newImg.src = images[i];  // Path is now relative to the root folder
   newImg.alt = 'Certificate ' + (i + 1);
 
-  // Event listener to change the main image when a thumbnail is clicked
+  // Add an event listener to change the main image when clicked
   newImg.addEventListener('click', () => {
     displayImg.src = newImg.src;
     displayImg.alt = newImg.alt;
   });
 
-  // Append the new image to the scroll container
+  // Append the new image to the scrollable container
   scrollDisplay.appendChild(newImg);
 }
